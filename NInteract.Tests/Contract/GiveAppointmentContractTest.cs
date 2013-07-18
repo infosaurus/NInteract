dc404d90c102adfd06f24161c9e59dfd347e011b
infosaurus <ianmcgog@gmail.com>
2013-07-18 22:04:04 +0200

    contract test

--- NInteract.Tests/Contract/GiveAppointmentContractTest.cs
+++ NInteract.Tests/Contract/GiveAppointmentContractTest.cs
@@ -3,23 +3,23 @@ using Ninteract.Tests.Collaboration;
 
 namespace Ninteract.Tests.Contract
 {
-    //public abstract class IAssistantContract
-    //{
-    //    [Test]
-    //    public void GiveAppointmentCanReturnNoAppointment()
-    //    {
-    //        var assistant = AssistantWithNoAppointment();
-    //        var appointment = assistant.GiveNextAppointment();
-    //        Assert.AreEqual(NoAppointment(), appointment);
-    //    }
+    public abstract class IAssistantContract
+    {
+        [Test]
+        public void GiveAppointmentCanReturnNoAppointment()
+        {
+            var assistant = AssistantWithNoAppointment();
+            var appointment = assistant.GiveNextAppointment();
+            Assert.AreEqual(NoAppointment(), appointment);
+        }
 
-    //    protected abstract IAssistant AssistantWithNoAppointment();
+        protected abstract IAssistant AssistantWithNoAppointment();
 
-    //    public static object NoAppointment()
-    //    {
-    //        return null;
-    //    }
-    //}
+        public static object NoAppointment()
+        {
+            return null;
+        }
+    }
 
     //[TestFixture]
     //public class AssistantContract : IAssistantContract
